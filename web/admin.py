f3bb3ccc3e1e8bb99978fdf8c09f32ad771cb523
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-03 16:50:36 -0500

    Parte_2

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Categoria,Producto
 
-# Register your models here.
+
+@admin.register(Categoria)
+class AdminCategoria(admin.ModelAdmin):
+    list_display= ('nombre','fecha_registro')
+    search_fields =('nombre',)
+    list_filter = ('nombre',)
+
+admin.site.register(Producto)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=150)
+    fecha_registro = models.DateField(auto_now_add=True) #Auto_now_add Se colocara la fecha cuando se cree el elemento
+
+    def __str__(self):
+        return self.nombre
+        
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT, blank=False, null=False) #Con el modo Restrict evitamos el borrado
+    nombre = models.CharField(max_length=150,verbose_name='Producto',blank=False,null=False)
+    descripcion = models.TextField(null=True,blank=True)
+    precio = models.DecimalField(max_digits=9, decimal_places=2)
+    fecha_registro = models.DateField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='productos/',blank=True)
+
+    def __str__(self):
+        return self.nombre
+
