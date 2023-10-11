e135e319db6479b96d7fd6ccafe3dbf782e54284
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-10-11 11:22:39 -0500

    XD

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# Ecommerce 🛒
-Proyecto de Ecommerce con Django y MySQL Con pasarela de pago con PayPal
-# Tecnologias 🐍
-Proyecto OPENSOURCE Realizado con Django y MySQL con idea de mejorar con el tiempo e implementar React , Angular o Vue y tambien TailwindCSS
-# Versiones
-Version 1.0
+# edshop
+Proyecto de E-Commerce con Django para Edteam
---
+++ web/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'web'
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria,name='productosPorCategoria'),
+    path('productosPorNombre',views.productosPorNombre,name='productosPorNombre'),
+    path('producto/<int:producto_id>',views.productoDetalle,name='producto'),
+    path('carrito',views.carrito,name='carrito'),
+    path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito'),
+    path('eliminarProductoCarrito/<int:producto_id>',views.eliminarProductoCarrito,name='eliminarProductoCarrito'),
+    path('limpiarCarrito',views.limpiarCarrito,name='limpiarCarrito'),
+    path('crearUsuario',views.crearUsuario,name='crearUsuario'),
+    path('cuenta',views.cuentaUsuario,name='cuentaUsuario'),
+    path('actualizarCliente',views.actualizarCliente,name='actualizarCliente'),
+    path('login/',views.loginUsuario,name='loginUsuario'),
+    path('logout/',views.logoutUsuario,name='logoutUsuario'),
+    path('registrarPedido',views.registrarPedido,name='registrarPedido'),
+    path('pruebapaypal',views.view_that_asks_for_money,name='pruebapaypal'),
+    path('confirmarPedido',views.confirmarPedido,name='confirmarPedido'),
+    path('gracias/',views.gracias,name='gracias')
+] 
